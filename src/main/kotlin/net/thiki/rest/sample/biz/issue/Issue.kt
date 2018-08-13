16e9e74ba38ab86c9ef29361dfc191972a978811
Joseph <zengzhu.cn@gmail.com>
2018-08-13 21:26:01 +0800

    assignee as a user

--- src/main/kotlin/net/thiki/rest/sample/biz/issue/Issue.kt
+++ src/main/kotlin/net/thiki/rest/sample/biz/issue/Issue.kt
@@ -3,7 +3,7 @@ package net.thiki.rest.sample.biz.issue
 class Issue(val id: Long, val key: String, val reporter: User, var summary: String) {
 
     var description: String? = null
-    var assignee: Long? = 0
+    var assignee: User? = null
     var status: Long? = 0
 
 
@@ -11,7 +11,7 @@ class Issue(val id: Long, val key: String, val reporter: User, var summary: Stri
     /**
      * constructor used by mybatis
      */
-    constructor(id: Long, key: String, reporter: User, summary: String, description: String?, assignee: Long?, status: Long?): this(id, key, reporter, summary){
+    constructor(id: Long, key: String, reporter: User, summary: String, description: String?, assignee: User?, status: Long?): this(id, key, reporter, summary){
         this.description = description
         this.assignee = assignee
         this.status = status
